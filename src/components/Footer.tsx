603546b6e5fa793d9acd9bfbb41c6415e8c24e6a
Lucas MESSIA <107100280+Lucasmes93@users.noreply.github.com>
2025-07-02 16:39:08 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => (
   <footer style={{ textAlign: 'center', padding: '1.2rem', background: '#5e2fc0', borderTop: '2px solid #5e2fc0', color: '#fff', fontWeight: 'bold', marginTop: '2rem', fontSize: '1rem', letterSpacing: '0.5px' }}>
-    © {new Date().getFullYear()} ESTIAM. Tous droits réservés.
+    © {new Date().getFullYear()} ESTIAM - HackSquad. Tous droits réservés.
   </footer>
 );
 
