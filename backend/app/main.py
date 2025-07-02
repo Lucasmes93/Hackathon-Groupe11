fd5dd3e01015bab4199fe73508ec3672ec736d9f
masterivanic <masterivanic@gmail.com>
2025-07-02 14:29:36 +0200

    chore: udpdate context data

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,6 +15,7 @@ import time
 from . import models, schemas, crud
 from .database import SessionLocal, engine
 from .logging import setup_logging
+from google.cloud import storage
 
 
 setup_logging()
@@ -115,8 +116,33 @@ async def get_uploaded_files(request: Request):
         raise HTTPException(status_code=500, detail=str(e))
     
 @app.get("/data", operation_id="estiam_data")
-async def get_context_data(request:Request):
-    return {"data": None}
+async def get_context_data(request: Request):
+    try:
+        storage_client = storage.Client()
+        bucket_name = "estiam_data_doc"
+        list_files = [
+            "Infos/Formations_ESTIAM.txt",
+            "Infos/Liste_des_campus_ESTIAM.txt",
+            "Infos/Presentation_ESTIAM.txt",
+            "Infos/faq_estiam.txt"
+        ]
+        
+        bucket = storage_client.bucket(bucket_name)
+        combined_content = ""
+        
+        for file_path in list_files:
+            try:
+                blob = bucket.blob(file_path)
+                combined_content += blob.download_as_text() + "\n\n"
+            except Exception:
+                continue 
+        return JSONResponse(content={"data": combined_content.strip()})
+    
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Failed to retrieve data: {str(e)}"
+        )
 
 
 include_operations_mcp.mount(mount_path="/include-operations-mcp")
